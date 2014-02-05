de24c27e29c959f1809a957b664ad88964819e5a
Seth Pollack <teampollack@gmail.com>
2014-02-05 01:12:34 -0500

    added location stuff

---
+++ Imzist.Web/Helpers/LocationResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Imzist.Data;
+
+namespace Imzist.Web.Helpers
+{
+    public static class LocationResolver
+    {
+        public static Location GetLocation()
+        {
+            var location = HttpContext.Current.Session["location"].ToString();
+            using (var db = new ImzistEntities())
+            {
+                return db.Locations.FirstOrDefault(l => l.Name == location);
+            }
+        }
+    }
+}
---
+++ Imzist.Web/LocationFilterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Imzist.Web
+{
+    public class LocationFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var location = filterContext.RouteData.Values["location"].ToString();
+            if (filterContext.HttpContext.Session != null) filterContext.HttpContext.Session["locaton"] = location;
+        }
+    }
+}
